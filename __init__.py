ec53f57428bb43a808584c795ba8c09d7975f26c
Edwin Roth <edwin.roth@gmx.at>
2021-05-24 00:53:03 +0200

    first steps

--- __init__.py
+++ __init__.py
@@ -11,21 +11,9 @@ class MycroftKodi(MycroftSkill):
 
     @intent_file_handler('kodi.mycroft.intent')
     def handle_kodi_mycroft(self, message):
-        self.speak_dialog('kodi.mycroft')
-
-    def converse(self, utterances, lang):
-            self.log.info(utterances[0])
-            self.log.info(utterances[1])
-
-            if utterances and self.voc_match(utterances[0], 'pegel'):
-                #self.speak_dialog('pegel')
-                self.speak('pegel')
-                return True
-            elif utterances and self.voc_match(utterances[0], 'pegel drei'):
-                self.speak_dialog('drei')
-                return True
-            else:
-                return False
+        volume = message.data.get('volume')
+        if volume is not None:
+            self.speak('volume)
 
 def create_skill():
     return MycroftKodi()
