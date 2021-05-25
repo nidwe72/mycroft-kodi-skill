4fa38ed9e993363d438fbfb75b4dc730741b329a
Edwin Roth <edwin.roth@gmx.at>
2021-05-25 19:56:04 +0200

    volume

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 from mycroft import MycroftSkill, intent_file_handler
-
+from kodijsonrpc import *
 
 class MycroftKodi(MycroftSkill):
 
@@ -9,10 +9,13 @@ class MycroftKodi(MycroftSkill):
     def initialize(self):
         self.register_entity_file('volume.entity')
 
-    @intent_file_handler('kodi.mycroft.intent')
+    @intent_file_handler('kodi.mycroft.volumne.intent')
     def handle_kodi_mycroft(self, message):
         volume = message.data.get('volume')
         if volume is not None:
+            server = KodiJSONClient('192.168.1.103', '8080', 'kodi', 'kodi')
+            kodiApplication=server.Application
+            kodiApplication.SetVolume(1)
             self.speak('die lautstärke von kodi wird auf '+volume+' gesetzt')
 
 def create_skill():
